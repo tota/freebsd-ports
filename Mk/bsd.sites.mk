tota/freebsd-ports

commit 7f4c8ec1e307e4b7f28fa3552421cda73a11c5cf

Fix a sub-submodule extraction problem with GH_SUBDIR.
The problem is that GH_SUBDIR are handled in a somewhat random manner.  (The
truth is that in the end of things, they end up being sorted by the group name
being used in the GH_TUPLE.)

So if you have a submodule in bar/foo, and a sub-submodule in bar/foo/baz, it
may happen that foo/bar/baz is handled before foo/bar and then things are
messed up.

This makes it so the GH_SUBDIR target handling is sorted first by the
number of / in the path.  (So, bar/foo is always handled before
bar/foo/baz.)

PR:		226221
Reported by:	ygy
Sponsored by:	Absolight
Differential Revision:	https://reviews.freebsd.org/D14532


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@463301 35697150-7ecd-e111-bb59-0022644237b5

Mk/bsd.sites.mk
@@ -486,7 +486,10 @@ DISTFILES:=	${DISTFILES} ${DISTFILE_${_group}}:${_group}
 MASTER_SITES:=	${MASTER_SITES} ${MASTER_SITE_GITHUB:S@%SUBDIR%@${GH_ACCOUNT_${_group}}/${GH_PROJECT_${_group}}/tar.gz/${GH_TAGNAME_${_group}}?dummy=/:${_group}@}
 WRKSRC_${_group}:=	${WRKDIR}/${GH_PROJECT_${_group}}-${GH_TAGNAME_${_group}_EXTRACT}
 .      if !empty(GH_SUBDIR_${_group})
-_SITES_extract:=	${_SITES_extract} 690:post-extract-gh-${_group}
+# In order to sort the subdir extraction so that foo/bar is moved in before
+# foo/bar/baz, we count the number of / in the path and use it to order the
+# targets.  This handles up to 9 levels.  The max as of r463123 is 4.
+_SITES_extract:=	${_SITES_extract} 69${GH_SUBDIR_${_group}:C=[^/]+= =g:range:[-1]}:post-extract-gh-${_group}
 post-extract-gh-${_group}:
 	@${RMDIR} ${WRKSRC}/${GH_SUBDIR_${_group}} 2>/dev/null || :
 	@${MKDIR} ${WRKSRC}/${GH_SUBDIR_${_group}:H} 2>/dev/null || :

commit 2abbaaaab44894ee36be1292b033121c6e63b3f8

Fix a regression on 10.3 introduced in r463301.
PR:		226221
Reported by:	mmokhi
Sponsored by:	Absolight


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@463628 35697150-7ecd-e111-bb59-0022644237b5

Mk/bsd.sites.mk
@@ -489,7 +489,7 @@ WRKSRC_${_group}:=	${WRKDIR}/${GH_PROJECT_${_group}}-${GH_TAGNAME_${_group}_EXTR
 # In order to sort the subdir extraction so that foo/bar is moved in before
 # foo/bar/baz, we count the number of / in the path and use it to order the
 # targets.  This handles up to 9 levels.  The max as of r463123 is 4.
-_SITES_extract:=	${_SITES_extract} 69${GH_SUBDIR_${_group}:C=[^/]+= =g:range:[-1]}:post-extract-gh-${_group}
+_SITES_extract:=	${_SITES_extract} 69${GH_SUBDIR_${_group}:C=[^/]+= =g:[#]}:post-extract-gh-${_group}
 post-extract-gh-${_group}:
 	@${RMDIR} ${WRKSRC}/${GH_SUBDIR_${_group}} 2>/dev/null || :
 	@${MKDIR} ${WRKSRC}/${GH_SUBDIR_${_group}:H} 2>/dev/null || :